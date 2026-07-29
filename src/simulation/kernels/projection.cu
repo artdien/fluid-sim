bed56645c64a332179147a96bf036ecc15fa60ce
artdien <92115972+artdien@users.noreply.github.com>
2026-07-29 11:57:29 +0200

    perf: use shared memory to speed up pressure solver

--- src/simulation/kernels/projection.cu
+++ src/simulation/kernels/projection.cu
@@ -17,14 +17,45 @@ __global__ auto calculate_divergence_kernel(GridView<f32> divergence, GridView<f
   }
 }
 
-__global__ auto solve_pressure_kernel(GridView<f32> pressure_next, GridView<f32> pressure, GridView<f32> divergence) -> void {
+__global__ auto solve_pressure_kernel(GridView<f32> pressure_next, GridView<f32> pressure, GridView<f32> divergence, u32 cache_size) -> void {
   const auto i {threadIdx.x + blockIdx.x * blockDim.x + 1};
   const auto j {threadIdx.y + blockIdx.y * blockDim.y + 1};
 
+  const auto l_i {threadIdx.x + 1};
+  const auto l_j {threadIdx.y + 1};
+
+  extern __shared__ f32 cache[];
+
+  // Plus three instead of plus two to avoid shared memory bank conflicts
+  const auto idx {[&](u32 i, u32 j) { return i + j * (cache_size + 3); }};
+
   if (i <= pressure.width && j <= pressure.height) {
-    pressure_next.at(i, j) =
-        (1.0f - jacobi_weight) * pressure.ro(i, j) +
-        jacobi_weight * (0.25f * (pressure.ro(i + 1, j) + pressure.ro(i - 1, j) + pressure.ro(i, j + 1) + pressure.ro(i, j - 1)) - divergence.ro(i, j));
+    cache[idx(l_i, l_j)] = pressure.ro(i, j);
+
+    if (l_i == 1) {
+      cache[idx(l_i - 1, l_j)] = pressure.ro(i - 1, j);
+    }
+    if (l_i == cache_size || i == pressure.width) {
+      cache[idx(l_i + 1, l_j)] = pressure.ro(i + 1, j);
+    }
+    if (l_j == 1) {
+      cache[idx(l_i, l_j - 1)] = pressure.ro(i, j - 1);
+    }
+    if (l_j == cache_size || j == pressure.height) {
+      cache[idx(l_i, l_j + 1)] = pressure.ro(i, j + 1);
+    }
+
+    __syncthreads();
+
+    const auto center {cache[idx(l_i, l_j)]};
+    const auto right {cache[idx(l_i + 1, l_j)]};
+    const auto left {cache[idx(l_i - 1, l_j)]};
+    const auto up {cache[idx(l_i, l_j + 1)]};
+    const auto down {cache[idx(l_i, l_j - 1)]};
+
+    const auto sum {right + left + up + down};
+
+    pressure_next.at(i, j) = (1.0f - jacobi_weight) * center + jacobi_weight * (0.25f * sum - divergence.ro(i, j));
   }
 }
 
@@ -52,8 +83,9 @@ auto calculate_divergence(GridView<f32> divergence, GridView<float2> velocity) -
 }
 
 auto solve_pressure(GridView<f32> pressure_next, GridView<f32> pressure, GridView<f32> divergence) -> void {
+  const auto cache_size {16u};
   const auto [blocks, threads] {utils::execution_configuration(pressure.width, pressure.height, 16)};
-  solve_pressure_kernel<<<blocks, threads>>>(pressure_next, pressure, divergence);
+  solve_pressure_kernel<<<blocks, threads, (cache_size + 3) * (cache_size + 2) * sizeof(f32)>>>(pressure_next, pressure, divergence, cache_size);
   utils::check_async_cuda_error();
 }
 
